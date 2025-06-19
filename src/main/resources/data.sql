c62b07dc2076821351ffc05116b4e707e8494169
덩엉이 <dud7757@naver.com>
2025-06-19 21:04:25 +0900

    JPA 로깅 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나나', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
+
+INSERT INTO member(email, password) VALUES('mail@mail.kr', '1111');
+INSERT INTO member(email, password) VALUES('mgasdail@saaa.kr', '2222');
+INSERT INTO member(email, password) VALUES('maigadsgal@maasdail.kr', '3333');
